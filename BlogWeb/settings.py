9466a9781240974a33a8415686ecd10918575967
Anil swami <anilswami8186@gmail.com>
2023-12-03 15:13:39 +0530

    Add Contact

--- BlogWeb/settings.py
+++ BlogWeb/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'Blog.apps.BlogConfig',
-    # 'Home.apps.HomeConfig',
+    'Blog.apps.BlogConfig',
+    'Home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Contact
 # Views for the Home page.
 def home(request):
     return render(request, "home/home.html")
@@ -8,4 +8,12 @@ def about(request):
     return render(request, "home/about.html")
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        contact = request.POST.get("contact")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        message = request.POST.get("message")
+        contact = Contact(name = name, contact = contact, email = email, password=password, message = message)
+        contact.save()
     return render(request, "home/contact.html")
